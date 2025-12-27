5b849277fd8c1de82259faf8c05847b6a3d7162a
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2025-12-27 19:22:13 +0100

    chore: add spinner to loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import './layout.css';
 	import favicon from '$lib/assets/lightning.svg';
 	import { notificationsSupported, subscribeAfterPermission } from './notifications.svelte';
+	import { Spinner } from '$lib/components/ui/spinner';
 
 	let { children } = $props();
 
@@ -27,7 +28,8 @@
 <main class="min-h-screen min-w-full flex flex-col items-center justify-center">
 	<svelte:boundary>
 		{#snippet pending()}
-			<div class="fixed inset-0 flex items-center justify-center backdrop-blur-sm">
+			<div class="fixed inset-0 flex items-center justify-center backdrop-blur-sm gap-2">
+				<Spinner class="text-primary size-6" />
 				<div class="animate-pulse font-medium">Loading…</div>
 			</div>
 		{/snippet}
